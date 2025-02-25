256db606ea3e2115eea049275720137cdc8e59e0
migraineman <migraineman@migraineman.net>
2025-02-24 19:04:09 -0600

    feat: add a string of numbers and a help option

--- Smart Calculator (Kotlin)/task/src/calculator/Main.kt
+++ Smart Calculator (Kotlin)/task/src/calculator/Main.kt
@@ -1,6 +1,23 @@
 package calculator
 
 fun main() {
-    val (x, y) = readln().split(" ").map {  it.toInt() }
-    println(x + y)
+    var input = ""
+    var integerList = mutableListOf<Int>()
+    while (input != "/exit") {
+         input = readln() ?: continue
+            if (input != "/exit") {
+                if (input == "/help") {
+                    println("The program calculates the sum of numbers")
+                } else {
+                try {
+                    integerList.clear()
+                    integerList.addAll(input.split(" ").map { it.toInt() })
+                } catch (e: NumberFormatException) {
+                    continue
+                }
+                println(integerList.sum())
+            }
+        }
+    }
+    println("Bye!")
 }
