366373ce75c6a903c8db5e4a037f53eb483bd918
migraineman <migraineman@migraineman.net>
2025-02-27 20:06:41 -0600

    feat: add subtraction support and multiple - or + signs

--- Smart Calculator (Kotlin)/task/src/calculator/Main.kt
+++ Smart Calculator (Kotlin)/task/src/calculator/Main.kt
@@ -2,20 +2,41 @@ package calculator
 
 fun main() {
     var input = ""
-    var integerList = mutableListOf<Int>()
+    var result = 0
     while (input != "/exit") {
-         input = readln() ?: continue
-            if (input != "/exit") {
-                if (input == "/help") {
-                    println("The program calculates the sum of numbers")
-                } else {
+        input = readln() ?: continue
+        if (input != "/exit" && !(input.isNullOrBlank())) {
+            if (input == "/help") {
+                println("The program calculates the sum and difference of numbers")
+            } else {
                 try {
-                    integerList.clear()
-                    integerList.addAll(input.split(" ").map { it.toInt() })
+                    var (operatorList: List<String>, numberList: List<String>) = input.split(" ")
+                        .partition { """[\+|\-]+""".toRegex().matches(it) }
+
+                    val mutableOperatorList = operatorList.toMutableList()
+                    // check for multiple minus or plus signs
+                    for (index in operatorList.indices) {
+                        if (operatorList[index].contains('-'))
+                            if (operatorList[index].length % 2 == 0) mutableOperatorList[index] = "+"
+                            else mutableOperatorList[index] = "-"
+                        if (operatorList[index].contains('+')) mutableOperatorList[index] = "+"
+                    }
+                    if (operatorList.size != numberList.size) {
+                        mutableOperatorList.add(0, "+")
+                        operatorList = mutableOperatorList.toList()
+                    }
+                    val integerList = numberList.map { it.toInt() }
+                    result = integerList.reduceIndexed { index, acc, i ->
+                        when (operatorList[index]) {
+                            "+" -> acc + i
+                            "-" -> acc - i
+                            else -> acc
+                        }
+                    }
                 } catch (e: NumberFormatException) {
                     continue
                 }
-                println(integerList.sum())
+                println(result)
             }
         }
     }
